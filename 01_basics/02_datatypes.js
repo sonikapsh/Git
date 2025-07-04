8c839e12c8764868ede4681f199dc8700619cce6
sonikapsh <soni.kapsh@gmail.com>
2025-07-04 15:36:26 +0000

    dataTypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3)  // we are using node.js not browser
+
+console.log(3+3)  // code readability should be high
+
+console.log("Kapil")
+
+let name = "kapil"
+let age = 18
+let isloggedin = false
+let state = null
+
+// number => 2 t0 power 53 
+// bigint
+//string => ""
+//boolean => true/false
+//null => standlone value
+//undefined =>  
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof "kapil")
+console.log(typeof age)
+console.log(typeof null) // obeject
+console.log(typeof undefined)  // undefined
