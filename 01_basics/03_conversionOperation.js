bea93832d288551a861660989f40efb235c7aa87
sonikapsh <soni.kapsh@gmail.com>
2025-07-12 13:34:40 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+// don't do this
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+// link to study 
+// https://tc39.es/ecma262/multipage/abstract-operatrions.html#sec-type-conversion
+
+let a = 2
+b = a++;
+c = ++a;
+console.log(a);
+console.log(b);
+console.log(c);
+// console.log(a : ${a});
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+console.log(2>1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
